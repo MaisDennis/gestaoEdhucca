62f4c09d592dd882fdf7e4281ddaee421e03b53b
Dennis Lee <dennisdjlee@yahoo.com>
2020-04-20 22:57:17 -0300

    Deploy adjustments.

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: 5435, // adicionar a porta se não estiver rodando a padrão 5432
+  port: 5432, // adicionar a porta se não estiver rodando a padrão 5432
   define: {
     timestamps: true, // saber quando registro foi criado/editado
     underscored: true,
